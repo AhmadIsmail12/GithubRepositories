3e28330f794b5df576d78f2186fd86a957a5d009
AhmadIsmail12 <ahmad.b.ismail1@gmail.com>
2025-08-04 16:52:52 +0300

    Initial Commit

---
+++ GithubRepository/GithubRepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepositoryApp.swift
+//  GithubRepository
+//
+//  Created by Ahmad Ismail on 04/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct GithubRepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithubRepositoryTests/GithubRepositoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepositoryTests.swift
+//  GithubRepositoryTests
+//
+//  Created by Ahmad Ismail on 04/08/2025.
+//
+
+import Testing
+@testable import GithubRepository
+
+struct GithubRepositoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
